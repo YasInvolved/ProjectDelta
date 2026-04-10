c43fe7716682fdd8211b3323751951a95ec35a5a
YasInvolved <jbaczyk27@gmail.com>
2026-04-11 00:32:36 +0200

    very basic cmake setup for initial development

---
+++ cmake/DeltaGame.cmake
@@ -0,0 +1,42 @@
+# Copyright 2026 Jakub Bączyk
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+function(setup_game GAME_TARGET)
+   set(GAME_BIN_DIR "${CMAKE_CURRENT_BINARY_DIR}/bin/$<CONFIG>")
+
+   set_target_properties(${GAME_TARGET} PROPERTIES
+      RUNTIME_OUTPUT_DIRECTORY "${GAME_BIN_DIR}"
+      LIBRARY_OUTPUT_DIRECTORY "${GAME_BIN_DIR}"
+      ARCHIVE_OUTPUT_DIRECTORY "${GAME_BIN_DIR}"
+      PREFIX ""
+   )
+
+   target_link_libraries(${GAME_TARGET} PRIVATE ProjectDelta)
+
+   add_custom_command(TARGET ${GAME_TARGET} POST_BUILD
+      COMMAND ${CMAKE_COMMAND} -E copy_if_different "$<TARGET_FILE:ProjectDelta>" "$<TARGET_FILE_DIR:${GAME_TARGET}>/$<TARGET_FILE_NAME:ProjectDelta>"
+      COMMAND ${CMAKE_COMMAND} -E copy_if_different "$<TARGET_FILE:DeltaLauncher>" "$<TARGET_FILE_DIR:${GAME_TARGET}>/$<TARGET_FILE_NAME:DeltaLauncher>"
+      COMMENT "Copying engine and game launcher to ${GAME_TARGET}'s bin directory..."
+   )
+
+   if (MSVC)
+      target_compile_options(${GAME_TARGET} PRIVATE /Zi)
+      set_target_properties(${GAME_TARGET} PROPERTIES
+         LINK_FLAGS "/DEBUG"
+         VS_DEBUGGER_COMMAND "${GAME_BIN_DIR}/DeltaLauncher.exe"
+         VS_DEBUGGER_WORKING_DIRECTORY "${GAME_BIN_DIR}"
+         VS_DEBUGGER_COMMAND_ARGUMENTS "$<TARGET_FILE_NAME:${GAME_TARGET}>"
+      )
+   endif()
+endfunction()
