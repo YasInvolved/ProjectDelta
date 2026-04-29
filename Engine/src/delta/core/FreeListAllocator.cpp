9ee96d3932622ba97ba8b8f42f00b9c46d7b1f22
YasInvolved <jbaczyk27@gmail.com>
2026-04-29 23:28:23 +0200

    segregated fit allocator (not finished, requires metadata implementation and deallocation routine)

---
+++ Engine/src/delta/core/FreeListAllocator.cpp
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2026 Jakub Bączyk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "FreeListAllocator.h"
+#include "MemoryManager.h"
+
+#define ALIGN(size, alignment) ((size + (alignment - 1)) & ~(size - 1))
+
+using namespace delta::core;
+
+using MemoryState = MemoryManager::MemoryState;
+using Node = FreeListAllocator::Node;
+
+DLT_FORCE_INLINE void* AllocatePageForBucket(FreeListAllocator* allocator, uint32_t bucketIx, uint64_t size)
+{
+    void* rawPage = MemoryManager::AllocatePageLockFree(allocator->memState);
+
+    uint64_t numChunks = allocator->pageSize / size;
+    uint8_t* walker = static_cast<uint8_t*>(rawPage);
+
+    Node* head = reinterpret_cast<Node*>(walker);
+    Node* current = head;
+
+    for (uint32_t i = 0; i < numChunks; i++)
+    {
+        walker += size;
+        Node* next = reinterpret_cast<Node*>(walker);
+        current->next = next;
+        current = next;
+    }
+
+    current->next = nullptr;
+    return head;
+}
+
+void delta::core::FreeList_Init(FreeListAllocator* allocator, MemoryState* memState)
+{
+    allocator->memState = memState;
+    allocator->pageSize = memState->pageSize;
+}
+
+void* delta::core::FreeList_Allocate(FreeListAllocator* allocator, uint64_t size, uint64_t alignment)
+{
+    uint64_t aligned = ALIGN(size, alignment);
+
+    if (aligned < FreeListAllocator::MIN_ALLOC_SIZE)
+        aligned = FreeListAllocator::MIN_ALLOC_SIZE;
+
+    if (aligned <= FreeListAllocator::MAX_BUCKET_SIZE)
+    {
+        uint32_t log = std::bit_width(aligned - 1);
+        uint32_t bucketIx = log - FreeListAllocator::INDEX_OFFSET;
+
+        Node* bucket = allocator->buckets[bucketIx];
+        if (bucket)
+        {
+            allocator->buckets[bucketIx] = bucket->next;
+            return bucket;
+        }
+
+        return AllocatePageForBucket(allocator, bucketIx, aligned);
+    }
+
+    return MemoryManager::AllocatePageLockFree(allocator->memState);
+}
---
+++ Engine/src/delta/core/FreeListAllocator.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2026 Jakub Bączyk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+namespace delta::core
+{
+    namespace MemoryManager { struct MemoryState; }
+
+    struct FreeListAllocator
+    {
+        static constexpr uint64_t INDEX_OFFSET = 4ull; // 2^4 = 16
+        static constexpr uint64_t MIN_ALLOC_SIZE = 16ull;
+        static constexpr uint64_t MAX_BUCKET_SIZE = 2048ull;
+        static constexpr uint32_t BUCKET_COUNT = 7ull; // log2(2048) - log2(16) = 7, calculated manually for simplicity
+
+        struct Node { Node* next; };
+
+        MemoryManager::MemoryState* memState;
+
+        Node* buckets[BUCKET_COUNT];
+
+        uint64_t pageSize;
+    };
+
+    void FreeList_Init(FreeListAllocator* allocator, MemoryManager::MemoryState* memState);
+    void* FreeList_Allocate(FreeListAllocator* allocator, uint64_t size, uint64_t alignment);
+}
