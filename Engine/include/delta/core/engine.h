722b35ef4371d388de32bc57bdfeb4d39a5e77e3
YasInvolved <jbaczyk27@gmail.com>
2026-05-30 20:53:38 +0200

    add new and delete operator overloads

--- Engine/include/delta/core/engine.h
+++ Engine/include/delta/core/engine.h
@@ -35,3 +35,13 @@ namespace delta::Engine
     [[nodiscard]] DLT_API void* Allocate(size_t size, AllocationType type, size_t alignment = 8) noexcept;
     DLT_API void Free(void* ptr) noexcept;
 }
+
+[[nodiscard]] inline void* operator new(size_t size) { return delta::Engine::Allocate(size, delta::Engine::AllocationType::PERSISTENT); }
+[[nodiscard]] inline void* operator new(size_t size, delta::Engine::AllocationType type) { return delta::Engine::Allocate(size, type); }
+[[nodiscard]] inline void* operator new[](size_t size) { return delta::Engine::Allocate(size, delta::Engine::AllocationType::PERSISTENT); }
+[[nodiscard]] inline void* operator new[](size_t size, delta::Engine::AllocationType type) { return delta::Engine::Allocate(size, type); }
+
+inline void operator delete(void* ptr) { return delta::Engine::Free(ptr); }
+inline void operator delete(void* ptr, delta::Engine::AllocationType) { return delta::Engine::Free(ptr); }
+inline void operator delete[](void* ptr) { return delta::Engine::Free(ptr); }
+inline void operator delete[](void* ptr, delta::Engine::AllocationType) { return delta::Engine::Free(ptr); }
